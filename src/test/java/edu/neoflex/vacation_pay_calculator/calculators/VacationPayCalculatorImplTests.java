ced7384b20f7fdf523a671b9c1e892aa9f7e990e
al3ksus <22aleksey08@mail.ru>
2024-09-20 22:23:41 +0300

    final

--- src/test/java/edu/neoflex/vacation_pay_calculator/calculators/VacationPayCalculatorImplTests.java
+++ src/test/java/edu/neoflex/vacation_pay_calculator/calculators/VacationPayCalculatorImplTests.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 
 @SpringBootTest
 public class VacationPayCalculatorImplTests {
@@ -16,6 +15,6 @@ public class VacationPayCalculatorImplTests {
     public void calculateTest() {
         VacationPayCalculator calculator = new VacationPayCalculatorImpl();
         Assertions.assertEquals(calculator.calculate(BigDecimal.valueOf(29300.0), 10), BigDecimal.valueOf(10000.0));
-        Assertions.assertEquals(calculator.calculate(BigDecimal.valueOf(100000.0), 28).setScale(0, RoundingMode.HALF_UP), BigDecimal.valueOf(95564));
+        Assertions.assertEquals(calculator.calculate(BigDecimal.valueOf(100000.0), 28), BigDecimal.valueOf(95564.0));
     }
 }
